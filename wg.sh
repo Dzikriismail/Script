64047c1f5d231a075d96b878c7189ce2f743b789
aryprabow <57415661+aryprabow@users.noreply.github.com>
2021-05-25 15:35:21 +0700

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -89,10 +89,10 @@ WG_RUNNING=$?
 
 # Tambahan
 cd /usr/bin
-wget -O add-wg "https://raw.githubusercontent.com/Nataslamet/Natascript/main/add-wg.sh"
-wget -O del-wg "https://raw.githubusercontent.com/Nataslamet/Natascript/main/del-wg.sh"
-wget -O cek-wg "https://raw.githubusercontent.com/Nataslamet/Natascript/main/cek-wg.sh"
-wget -O renew-wg "https://raw.githubusercontent.com/Nataslamet/Natascript/main/renew-wg.sh"
+wget -O add-wg "https://raw.githubusercontent.com/aryprabow/Script/main/add-wg.sh"
+wget -O del-wg "https://raw.githubusercontent.com/aryprabow/Script/main/del-wg.sh"
+wget -O cek-wg "https://raw.githubusercontent.com/aryprabow/Script/main/cek-wg.sh"
+wget -O renew-wg "https://raw.githubusercontent.com/aryprabow/Script/main/renew-wg.sh"
 chmod +x add-wg
 chmod +x del-wg
 chmod +x cek-wg
