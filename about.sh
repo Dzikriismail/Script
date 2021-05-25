24c99cd29ab5e2b3a01b8eb5d47d0ce0d3a6be29
aryprabow <57415661+aryprabow@users.noreply.github.com>
2021-05-25 13:47:56 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,7 +2,7 @@
 
 clear
 echo -e "================================================="
-echo -e "#       This Script Edited By Nataserver        #"
+echo -e "#       This Script Edited By Bowo              #"
 echo -e "#-----------------------------------------------#"
 echo -e "# For Debian 9 & Debian 10 64 bit               #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
