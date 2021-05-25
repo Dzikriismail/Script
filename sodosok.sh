747f521c684db534b9a0618d052809c7225d2836
aryprabow <57415661+aryprabow@users.noreply.github.com>
2021-05-25 15:02:47 +0700

    Update sodosok.sh

--- sodosok.sh
+++ sodosok.sh
@@ -88,10 +88,10 @@ iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 cd /usr/bin
-wget -O add-ss "https://raw.githubusercontent.com/Nataslamet/Natascript/main/add-ss.sh"
-wget -O del-ss "https://raw.githubusercontent.com/Nataslamet/Natascript/main/del-ss.sh"
-wget -O cek-ss "https://raw.githubusercontent.com/Nataslamet/Natascript/main/cek-ss.sh"
-wget -O renew-ss "https://raw.githubusercontent.com/Nataslamet/Natascript/main/renew-ss.sh"
+wget -O add-ss "https://raw.githubusercontent.com/aryprabow/Script/main/add-ss.sh"
+wget -O del-ss "https://raw.githubusercontent.com/aryprabow/Script/main/del-ss.sh"
+wget -O cek-ss "https://raw.githubusercontent.com/aryprabow/Script/main/cek-ss.sh"
+wget -O renew-ss "https://raw.githubusercontent.com/aryprabow/Script/main/renew-ss.sh"
 chmod +x add-ss
 chmod +x del-ss
 chmod +x cek-ss
