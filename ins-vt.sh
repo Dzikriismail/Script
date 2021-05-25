0ff874b21ac4653e9fe1422dc91c5d377f28f668
aryprabow <57415661+aryprabow@users.noreply.github.com>
2021-05-25 14:29:08 +0700

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -16,7 +16,7 @@ date
 mkdir -p /etc/trojan/
 touch /etc/trojan/akun.conf
 # install v2ray
-wget https://raw.githubusercontent.com/Nataslamet/Natascript/main/go.sh && chmod +x go.sh && ./go.sh
+wget https://raw.githubusercontent.com/aryprabow/Script/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
 bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
 mkdir /root/.acme.sh
@@ -491,19 +491,19 @@ systemctl enable trojan
 systemctl restart v2ray
 systemctl enable v2ray
 cd /usr/bin
-wget -O add-ws "https://raw.githubusercontent.com/Nataslamet/Natascript/main/add-ws.sh"
-wget -O add-vless "https://raw.githubusercontent.com/Nataslamet/Natascript/main/add-vless.sh"
-wget -O add-tr "https://raw.githubusercontent.com/Nataslamet/Natascript/main/add-tr.sh"
-wget -O del-ws "https://raw.githubusercontent.com/Nataslamet/Natascript/main/del-ws.sh"
-wget -O del-vless "https://raw.githubusercontent.com/Nataslamet/Natascript/main/del-vless.sh"
-wget -O del-tr "https://raw.githubusercontent.com/Nataslamet/Natascript/main/del-tr.sh"
-wget -O cek-ws "https://raw.githubusercontent.com/Nataslamet/Natascript/main/cek-ws.sh"
-wget -O cek-vless "https://raw.githubusercontent.com/Nataslamet/Natascript/main/cek-vless.sh"
-wget -O cek-tr "https://raw.githubusercontent.com/Nataslamet/Natascript/main/cek-tr.sh"
-wget -O renew-ws "https://raw.githubusercontent.com/Nataslamet/Natascript/main/renew-ws.sh"
-wget -O renew-vless "https://raw.githubusercontent.com/Nataslamet/Natascript/main/renew-vless.sh"
-wget -O renew-tr "https://raw.githubusercontent.com/Nataslamet/Natascript/main/renew-tr.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/Nataslamet/Natascript/main/cert.sh"
+wget -O add-ws "https://raw.githubusercontent.com/aryprabow/Script/main/add-ws.sh"
+wget -O add-vless "https://raw.githubusercontent.com/aryprabow/Script/main/add-vless.sh"
+wget -O add-tr "https://raw.githubusercontent.com/aryprabow/Script/main/add-tr.sh"
+wget -O del-ws "https://raw.githubusercontent.com/aryprabow/Script/main/del-ws.sh"
+wget -O del-vless "https://raw.githubusercontent.com/aryprabow/Script/main/del-vless.sh"
+wget -O del-tr "https://raw.githubusercontent.com/aryprabow/Script/main/del-tr.sh"
+wget -O cek-ws "https://raw.githubusercontent.com/aryprabow/Script/main/cek-ws.sh"
+wget -O cek-vless "https://raw.githubusercontent.com/aryprabow/Script/main/cek-vless.sh"
+wget -O cek-tr "https://raw.githubusercontent.com/aryprabow/Script/main/cek-tr.sh"
+wget -O renew-ws "https://raw.githubusercontent.com/aryprabow/Script/main/renew-ws.sh"
+wget -O renew-vless "https://raw.githubusercontent.com/aryprabow/Script/main/renew-vless.sh"
+wget -O renew-tr "https://raw.githubusercontent.com/aryprabow/Script/main/renew-tr.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/aryprabow/Script/main/cert.sh"
 chmod +x add-ws
 chmod +x add-vless
 chmod +x add-tr
